7de45f67fc804e48943a09f584f0e8036fb15d89
PizzaPalace <fullyautomatix.inc@gmail.com>
2015-12-24 08:06:49 +0530

    ui

--- app/src/main/java/com/assignment/gre/activities/MainActivity.java
+++ app/src/main/java/com/assignment/gre/activities/MainActivity.java
@@ -1,19 +1,62 @@
 package com.assignment.gre.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.assignment.gre.R;
+import com.assignment.gre.fragments.ContentFragment;
+import com.assignment.gre.fragments.NavigationDrawerFragment;
+import com.assignment.gre.views.SlidingTabLayout;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                          implements NavigationDrawerFragment.OnFragmentInteractionListener,
+                                     ContentFragment.OnFragmentInteractionListener{
+
+
+    private static int count = 3;
+
+    private Toolbar mToolbar;
+    private NavigationDrawerFragment mDrawerFragment;
+    private DrawerLayout mDrawerLayout;
+    private ViewPager mViewPager;
+    private SlidingTabLayout mTabs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        mDrawerFragment.intializeDrawer(R.id.fragment_navigation_drawer,mDrawerLayout,mToolbar);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new SlidingPagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.sliding_tab_layout);
+        mTabs.setCustomTabView(R.layout.tab_view, R.id.tabText);
+        //make sure all tabs take the full horizontal screen space and divide it equally amongst themselves
+        mTabs.setDistributeEvenly(true);
+        mTabs.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        //color of the tab indicator
+        mTabs.setSelectedIndicatorColors(getResources().getColor(R.color.colorAccent));
+        mTabs.setViewPager(mViewPager);
     }
 
 
@@ -37,5 +80,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onContentFragmentInteraction(Uri uri) {
+
+    }
+
+    private class SlidingPagerAdapter extends FragmentStatePagerAdapter{
+
+        private String[] tabText;
+
+        public SlidingPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabText = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            ContentFragment fragment = ContentFragment.newInstance("","");
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
     }
 }
--- app/src/main/java/com/assignment/gre/network/VolleySingleton.java
+++ app/src/main/java/com/assignment/gre/network/VolleySingleton.java
@@ -1,8 +1,9 @@
 package com.assignment.gre.network;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.LruCache;
-
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
@@ -13,36 +14,54 @@ import com.android.volley.toolbox.Volley;
  */
 public class VolleySingleton {
 
-    private static VolleySingleton sInstance=null;
-    private ImageLoader mImageLoader;
+    private static VolleySingleton mInstance;
     private RequestQueue mRequestQueue;
-    private VolleySingleton(){
-        mRequestQueue=Volley.newRequestQueue(/*insert context*/);
-        mImageLoader=new ImageLoader(mRequestQueue,new ImageLoader.ImageCache() {
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
 
-            private LruCache<String, Bitmap> cache=new LruCache<>((int)(Runtime.getRuntime().maxMemory()/1024)/8);
-            @Override
-            public Bitmap getBitmap(String url) {
-                return cache.get(url);
-            }
+    private VolleySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
 
-            @Override
-            public void putBitmap(String url, Bitmap bitmap) {
-                cache.put(url, bitmap);
-            }
-        });
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
     }
-    public static VolleySingleton getInstance(){
-        if(sInstance==null)
-        {
-            sInstance=new VolleySingleton();
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
         }
-        return sInstance;
+        return mInstance;
     }
-    public RequestQueue getRequestQueue(){
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
         return mRequestQueue;
     }
-    public ImageLoader getImageLoader(){
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
         return mImageLoader;
     }
+
 }
